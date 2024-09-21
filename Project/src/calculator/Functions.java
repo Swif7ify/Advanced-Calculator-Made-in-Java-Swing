9306c0085a7577fd7374121dafec4cf65ab7b15c
Swif7ify <earl.coding@gmail.com>
2024-09-21 12:24:30 +0800

    FIXED REDUNDANCY

--- Project/src/calculator/Functions.java
+++ Project/src/calculator/Functions.java
@@ -1,56 +1,118 @@
-//package calculator;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//import java.awt.image.BufferedImage;
-//import java.awt.image.ImageObserver;
-//import java.io.File;
-//import java.io.IOException;
-//import javax.imageio.ImageIO;
-//
-//public class Functions {
-//    public static void main(String[] args) {
-//        // Create the button
-//        RoundedButton cuberootButton = new RoundedButton("", 50);
-//
-//        // Load and scale the image with anti-aliasing
-//        ImageIcon scaledIcon = createScaledImageIcon("C:\\Users\\earlo\\OneDrive\\Desktop\\Calculator Project\\Project\\Picture\\cuberoot_icon.png", 50, 50);
-//        cuberootButton.setIcon(scaledIcon);
-//
-//        // Set font and add action listener
-//        cuberootButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
-//        cuberootButton.addActionListener(e -> {
-//            // Handle button click
-//            JOptionPane.showMessageDialog(null, "Button Clicked");
-//        });
-//
-//        // Example JFrame setup to display the button
-//        JFrame frame = new JFrame();
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setLayout(new FlowLayout());
-//        frame.add(cuberootButton);
-//        frame.pack();
-//        frame.setVisible(true);
-//    }
-//
-//    private static ImageIcon createScaledImageIcon(String filePath, int width, int height) {
-//        try {
-//            BufferedImage originalImage = ImageIO.read(new File(filePath));
-//            BufferedImage scaledImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-//            Graphics2D g2d = scaledImage.createGraphics();
-//            
-//            // Apply anti-aliasing
-//            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-//            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-//            
-//            // Draw the scaled image
-//            g2d.drawImage(originalImage, 0, 0, width, height, null);
-//            g2d.dispose();
-//            
-//            return new ImageIcon(scaledImage);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-//    }
-//}
+package calculator;
+
+public class Functions {
+	public static int factorial(double firstValue) {
+		int Answer;
+		if(firstValue == 0 || firstValue == 1) {
+			Answer = 1;
+		} else {
+			Answer = 1;
+			for (int i = 2; i <= firstValue; i++) {
+				Answer *= i; 
+			}
+		}
+		return Answer;
+	}
+	
+	public static int sumFactorial(double firstValue, double secondValue) {
+		return Functions.factorial(firstValue) + Functions.factorial(secondValue);
+	}
+	
+	public static int divideFactorial(double firstValue, double secondValue) {
+		return Functions.factorial(firstValue) / Functions.factorial(secondValue);
+	}
+	
+	public static double summation(double firstValue, double secondValue, double thirdValue) {
+		int start = (int) firstValue;
+		int end = (int) secondValue;
+		int constant = (int) thirdValue;
+		int sum = 0;
+		if (constant == 0 || constant == 1) {
+			for (int n = start; n <= end; n++) {
+				sum += n;
+			}
+		} else {
+			for (int n = start; n <= end; n++) {
+				sum +=constant;
+        	}
+		}
+		return sum;
+	}
+	
+	public static double doubleSummation(double firstValue, double secondValue, double thirdValue, double fourthValue) {
+		int start = (int) firstValue;
+		int end = (int) secondValue;
+		int jstart = (int) thirdValue;
+		int jend = (int) fourthValue;
+		int sum = 0;
+		
+		for(int n = start; n <= end; n++) {
+			for(int j = jstart; j <= jend; j++) {
+				sum += (3*n*j);
+			}
+		}
+		return sum;
+	}
+	
+	public static int prodnot(double firstValue, double secondValue, double thirdValue) {
+		int start = (int) firstValue;
+		int end = (int) secondValue;
+		int constant = (int) thirdValue;
+		int sum = 1;
+		if (constant == 0 || constant == 1) {
+			for (int n = start; n <= end; n++) {
+				sum *= n;
+			}
+		} else {
+			for (int n = start; n <= end; n++) {
+				sum *= constant;
+        	}
+		}
+		return sum;
+	}
+	
+	public static double doubleProdNot(double firstValue, double secondValue, double thirdValue, double fourthValue) {
+		int start = (int) firstValue;
+		int end = (int) secondValue;
+		int jstart = (int) thirdValue;
+		int jend = (int) fourthValue;
+		int sum = 1;
+		
+		for(int n = start; n <= end; n++) {
+			for(int j = jstart; j <= jend; j++) {
+				sum *= (3*n*j);
+			}
+		}
+		return sum;
+	}
+	
+	public static double basicCalculation(String operator, double firstValue, double secondValue) {
+		double Answer = 0;
+		switch(operator) {
+		case "+":
+			Answer = firstValue + secondValue;
+			break;
+		case "-":
+			Answer = firstValue - secondValue;
+			break;
+		case "*":
+			Answer = firstValue * secondValue;
+			break;
+		case "÷":
+			Answer = firstValue / secondValue;
+			break;
+		case "//":
+			Answer = firstValue / secondValue;
+			Answer = (int) Answer;
+			break;
+		case "%":
+			Answer = firstValue % secondValue;
+			break;
+		case "x^y":
+			Answer = Math.pow(firstValue, secondValue);
+			break;
+		}
+		
+		return Answer;
+	}
+}
