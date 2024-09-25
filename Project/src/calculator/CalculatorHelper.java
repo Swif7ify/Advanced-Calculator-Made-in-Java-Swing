82752ab20e6aebeaf3663abdbc841e887da1d277
Swif7ify <earl.coding@gmail.com>
2024-09-25 22:01:05 +0800

    Fixed Formatted String & Added New helper

--- Project/src/calculator/CalculatorHelper.java
+++ Project/src/calculator/CalculatorHelper.java
@@ -2,6 +2,8 @@ package calculator;
 
 import java.util.ArrayList;
 
+import javax.swing.JLabel;
+
 public class CalculatorHelper {
 	
 	private RoundJTextField calc;
@@ -10,12 +12,14 @@ public class CalculatorHelper {
 	private RoundJTextField zValue;
 	private RoundJTextField yValue;
 	private RoundJTextField xValue;
+	private JLabel xy_holder;
+	private JLabel xyz_holder;
 	
 	private ArrayList<Double> numbers;
 	private ArrayList<String> operators;
 	
 	public CalculatorHelper(RoundJTextField calc, RoundJTextField numwrapper, RoundJTextField holder,
-			RoundJTextField zValue, RoundJTextField yValue, RoundJTextField xValue ) {
+			RoundJTextField zValue, RoundJTextField yValue, RoundJTextField xValue, JLabel xy_holder, JLabel xyz_holder ) {
 		
 		this.calc = calc;
 		this.numwrapper = numwrapper;
@@ -23,6 +27,8 @@ public class CalculatorHelper {
 		this.zValue = zValue;
 		this.yValue = yValue;
 		this.xValue = xValue;
+		this.xy_holder = xy_holder;
+		this.xyz_holder = xyz_holder;
 		
 		numbers = new ArrayList<>();
 		operators = new ArrayList<>();
@@ -64,4 +70,25 @@ public class CalculatorHelper {
         numbers.clear();
         operators.clear();
     }
+	
+	public void setXYZInactive() {
+		zValue.setBounds(0, 0, 0, 0);
+		yValue.setBounds(0, 0, 0, 0);
+		xValue.setBounds(0, 0, 0, 0);
+		xyz_holder.setBounds(0, 0, 0, 0);
+		xy_holder.setBounds(0, 0, 0, 0);
+	}
+	
+	public void setXYActive() {
+		yValue.setBounds(515, 36, 178, 25);
+		xValue.setBounds(515, 54, 161, 41);
+		xy_holder.setBounds(15, 37, 66, 60);
+	}
+	
+	public void setXYZActive() {
+		zValue.setBounds(500, 21, 189, 25);
+		yValue.setBounds(497, 46, 178, 27);
+		xValue.setBounds(497, 67, 161, 41);
+		xyz_holder.setBounds(15, 37, 66, 60);
+	}
 }
